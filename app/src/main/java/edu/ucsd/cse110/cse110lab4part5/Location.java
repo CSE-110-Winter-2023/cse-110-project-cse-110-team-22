5fde17440c4f70c76f5f8b6ffe9d65677ca6d6ce
yug030 <itsygao@gmail.com>
2023-02-08 10:59:44 +0800

    Location interface

---
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/Location.java
@@ -0,0 +1,8 @@
+package edu.ucsd.cse110.cse110lab4part5;
+
+public interface Location {
+    public double getLongitude();
+    public double getLatitude();
+    public double setLongitude();
+    public double setLatitude();
+}
