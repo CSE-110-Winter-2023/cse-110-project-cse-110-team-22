6af0621fc96c3a0d4f50818fdc7be073666f2664
yug030 <itsygao@gmail.com>
2023-02-10 12:45:43 +0800

    Created OrientationService, still need to edit MainActivity

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private UserOrientationService orientationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        orientationService = new UserOrientationService(this);
+        TextView txt = findViewById(R.id.orientationText);
     }
 
     public void goToRotate(View view) {
@@ -22,5 +28,11 @@ public class MainActivity extends AppCompatActivity {
     public void goToClock(View view) {
         Intent intent = new Intent(this, ClockActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        orientationService.unregisterSensorListeners();
     }
 }
