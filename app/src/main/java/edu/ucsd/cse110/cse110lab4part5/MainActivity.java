1866b4ecb5ade5dedc356ca82ee77ae74b7dcbd7
seanjiang-0416 <zhixing.jiang.sean@gmail.com>
2023-02-11 20:09:07 -0800

    Location_compass

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
@@ -35,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
         locationService.getLocation().observe(this, loc ->{
             test.setText(Double.toString(loc.first) + " , "+Double.toString(loc.second));
         });
+
+
+
+
     }
     public void submit_alert(View view) {
         Utilities.showAlert(this, "Testing");
---
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/clockActivity.java
@@ -0,0 +1,13 @@
+package edu.ucsd.cse110.cse110lab4part5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class clockActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+}
