b58e92b3e22c90fd84610662692b66ce2b84c88a
skyeyestang99 <tommytang9958@gmail.com>
2023-02-11 19:20:25 -0800

    added location service and textbox for testing

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
@@ -1,20 +1,42 @@
 package edu.ucsd.cse110.cse110lab4part5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.MutableLiveData;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MutableLiveData<Pair<Double, Double>> locationValue;
+    private LocationService locationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_coordinate);
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},200);
+        }
+
+        locationService = LocationService.singleton(this);
+
+        TextView test = (TextView) findViewById(R.id.serviceTextView);
+
+        locationService.getLocation().observe(this, loc ->{
+            test.setText(Double.toString(loc.first) + " , "+Double.toString(loc.second));
+        });
     }
     public void submit_alert(View view) {
         Utilities.showAlert(this, "Testing");
     }
-
 }
