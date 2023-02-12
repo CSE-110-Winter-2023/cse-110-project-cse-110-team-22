7f3ed8404ea0858efe8c09002e804aa5c9885464
Jonathan Weakliem <67931921+jdweak@users.noreply.github.com>
2023-02-12 15:43:13 -0800

    initial linking of features (input, compassUI)

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/CompassActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/CompassActivity.java
@@ -10,7 +10,32 @@ public class CompassActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_clock);
+        setContentView(R.layout.activity_compass);
+
+
+        // get location data
+        Bundle extras = getIntent().getExtras();
+        Location familyLocation = new LandmarkLocation(extras.getDouble("family_longitude"),
+                extras.getDouble("family_latitude"),
+                extras.getString("family_label"));
+
+        Location friendLocation = new LandmarkLocation(extras.getDouble("friend_longitude"),
+                extras.getDouble("friend_latitude"),
+                extras.getString("friend_label"));
+
+        Location homeLocation = new LandmarkLocation(extras.getDouble("home_longitude"),
+                extras.getDouble("home_latitude"),
+                extras.getString("home_label"));
+
+        // Hardcoded user location for demo purposes, WIP
+        // Location is UCSD center campus facing north (For now we are ignoring user orientation)
+        Location userLocation = new UserLocation(32.88014354083708, -117.2318005216365, "selfLocation");
+
+        // update location data
+        updateCircleAngle(R.id.familyhouse, (float)LocationUtils.computeAngle(userLocation, familyLocation));
+        updateCircleAngle(R.id.friend, (float)LocationUtils.computeAngle(userLocation, friendLocation));
+        updateCircleAngle(R.id.home, (float)LocationUtils.computeAngle(userLocation, homeLocation));
+
     }
     private void updateCircleAngle(int imageViewId, float angle) {
         ImageView imageView = findViewById(imageViewId);
