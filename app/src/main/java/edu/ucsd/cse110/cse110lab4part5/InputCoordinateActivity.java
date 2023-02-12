2b8eb770596559e8afcae24c05aad549155243a0
Jonathan Weakliem <67931921+jdweak@users.noreply.github.com>
2023-02-12 11:13:16 -0800

    refactor class name to follow cammel case

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/InputCoordinateActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/InputCoordinateActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class inputCoordinateActivity extends AppCompatActivity {
+public class InputCoordinateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
