4a43aaad71d0530d7e43e1081df231282840c26f
yug030 <itsygao@gmail.com>
2023-02-12 14:43:21 +0800

    Passing user inputs after clicking submit button

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
@@ -6,13 +6,16 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Optional;
+
+public class  MainActivity extends AppCompatActivity {
 
     private MutableLiveData<Pair<Double, Double>> locationValue;
     private UserLocationService userLocationService;
@@ -39,8 +42,53 @@ public class MainActivity extends AppCompatActivity {
             test.setText(Double.toString(loc.first) + " , "+Double.toString(loc.second));
         });
     }
-    public void submit_alert(View view) {
-        Utilities.showAlert(this, "Testing");
+    public void submit_button(View view) {
+        //TODO if
+        //Utilities.showAlert(this, "Testing");
+
+        Intent intent = new Intent(this, CompassActivity.class);
+        putIntent(intent);
+        startActivity(intent);
+    }
+
+    private void putIntent(Intent intent) {
+        TextView home_longitude_view = findViewById(R.id.longitude_home);
+        TextView home_latitude_view = findViewById(R.id.latitude_home);
+        TextView family_longitude_view = findViewById(R.id.longitude_family);
+        TextView family_latitude_view = findViewById(R.id.latitude_family);
+        TextView friend_longitude_view = findViewById(R.id.longitude_friend);
+        TextView friend_latitude_view = findViewById(R.id.latitude_friend);
+
+        String home_longitude_str = home_longitude_view.getText().toString();
+        String home_latitude_str = home_latitude_view.getText().toString();
+        String family_longitude_str = family_longitude_view.getText().toString();
+        String family_latitude_str = family_latitude_view.getText().toString();
+        String friend_longitude_str = friend_longitude_view.getText().toString();
+        String friend_latitude_str = friend_latitude_view.getText().toString();
+
+        double home_longitude_val = Double.parseDouble(home_longitude_str);
+        double home_latitude_val = Double.parseDouble(home_latitude_str);
+        double friend_longitude_val = Double.parseDouble(friend_longitude_str);
+        double friend_latitude_val = Double.parseDouble(friend_latitude_str);
+        double family_longitude_val = Double.parseDouble(family_longitude_str);
+        double family_latitude_val = Double.parseDouble(family_latitude_str);
+//        Optional<Integer> maybeMaxCount = Utilities.parseCount(maxCountStr);
+//        if (!maybeMaxCount.isPresent()) {
+//            Utilities.showAlert(this, "That isn't a number!");
+//            return;
+//        }
+//        int maxCount = maybeMaxCount.get();
+//        if (maxCount <= 0) {
+//            Utilities.showAlert(this, "Please enter a positive number!");
+//            return;
+//        }
+
+        intent.putExtra("home_longitude", home_longitude_val);
+        intent.putExtra("home_latitude", home_latitude_val);
+        intent.putExtra("friend_longitude", friend_longitude_val);
+        intent.putExtra("friend_latitude", friend_latitude_val);
+        intent.putExtra("family_longitude", family_longitude_val);
+        intent.putExtra("family_latitude", family_latitude_val);
     }
 
     @Override
