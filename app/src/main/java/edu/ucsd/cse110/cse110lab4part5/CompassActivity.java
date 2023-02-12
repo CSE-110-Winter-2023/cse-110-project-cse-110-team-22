7efe3bcc09bd1c8ceab2c5320b3f4d3116194179
Jonathan Weakliem <67931921+jdweak@users.noreply.github.com>
2023-02-12 12:04:55 -0800

    added compassUI update method

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/CompassActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/CompassActivity.java
@@ -1,13 +1,21 @@
 package edu.ucsd.cse110.cse110lab4part5;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class CompassActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clock);
+    }
+    private void updateCircleAngle(int imageViewId, float angle) {
+        ImageView imageView = findViewById(imageViewId);
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) imageView.getLayoutParams();
+        layoutParams.circleAngle = angle;
+        imageView.setLayoutParams(layoutParams);
     }
 }
