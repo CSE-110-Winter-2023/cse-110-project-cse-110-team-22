f6c308cb182fc35e271c5cadc8f5f05ddff87dd6
seanjiang-0416 <zhixing.jiang.sean@gmail.com>
2023-02-11 21:28:59 -0800

    test

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_input_coordinate);
+        setContentView(R.layout.activity_clock);
 
         if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED){
