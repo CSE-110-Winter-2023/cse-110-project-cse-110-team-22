c364e2ae30aec62727a88ea63ae82e1d723b5fe9
Maximilian Wang <maxone2011@gmail.com>
2023-02-18 17:23:53 -0800

    sample test

---
+++ app/src/test/java/edu/ucsd/cse110/cse110lab4part5/DataPersistenceTest.java
@@ -0,0 +1,19 @@
+package edu.ucsd.cse110.cse110lab4part5;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class DataPersistenceTest {
+    @Test
+    public void TestAdd(){
+        assertEquals(1, 1);
+    }
+}
