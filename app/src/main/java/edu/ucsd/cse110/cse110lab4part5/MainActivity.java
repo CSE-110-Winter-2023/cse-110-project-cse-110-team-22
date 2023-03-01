7d26873898184e673cfc054cbd13b911123f4afd
seanjiang-0416 <zhixing.jiang.sean@gmail.com>
2023-03-01 00:11:09 -0800

    s1#76 hook up UI pages with testings

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/MainActivity.java
@@ -10,6 +10,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
@@ -23,10 +24,10 @@ import java.util.Optional;
 
 public class  MainActivity extends AppCompatActivity {
 
-    private MutableLiveData<Pair<Double, Double>> locationValue;
-    private UserLocationService userLocationService;
-    private UserOrientationService orientationService;
-    private UserLocation userLocation;
+//    private MutableLiveData<Pair<Double, Double>> locationValue;
+//    private UserLocationService userLocationService;
+//    private UserOrientationService orientationService;
+//    private UserLocation userLocation;
 
 
     @Override
@@ -34,21 +35,23 @@ public class  MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // Handle location permissions
-        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED){
-            Log.d("MainActivity", "Asking for location permissions");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},200);
-        }
+//        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED){
+//            Log.d("MainActivity", "Asking for location permissions");
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},200);
+//        }
 
 
         // check if locations exist already. If so pass a flag that will indicate to InputCoordinateActivity
         // that it should go straight to compass based on stored data
-        Intent intent = new Intent(this, InputCoordinateActivity.class);
+//        Intent intent = new Intent(this, InputCoordinateActivity.class);
+//
+//        if(SharedPrefUtils.hasStoredLocations(this)){
+//            intent.putExtra("activity_flag", 0);
+//        }
+//        startActivity(intent);
 
-        if(SharedPrefUtils.hasStoredLocations(this)){
-            intent.putExtra("activity_flag", 0);
-        }
+        Intent intent = new Intent(this, input_name.class);
         startActivity(intent);
-
     }
 }
