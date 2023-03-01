205a6bd594bed5d873eec81e7c30dd864d6a0ea6
Jonathan Weakliem <67931921+jdweak@users.noreply.github.com>
2023-02-28 18:20:32 -0800

    #99 friend class

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/Friend.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/Friend.java
@@ -1,4 +1,21 @@
 package edu.ucsd.cse110.cse110lab4part5;
 
 public class Friend {
+    Location myLocation;
+    final String name;
+    final String uuid;
+
+    public Friend(String name, String uuid){
+        this.name = name;
+        this.uuid = uuid;
+    }
+    public void setLocation(Location l){
+        if (l == null) {
+            throw new NullPointerException();
+        }
+        myLocation = l;
+    }
+    public Location getLocation() {
+        return myLocation;
+    }
 }
