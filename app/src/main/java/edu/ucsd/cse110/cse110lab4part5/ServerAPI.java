bf08ca737a904a9240aa0500f81c3830e4e960cb
Jonathan Weakliem <67931921+jdweak@users.noreply.github.com>
2023-03-02 19:55:50 -0800

    #77 api now points at new server, better threading, upsert/get/checkuuid methods

--- app/src/main/java/edu/ucsd/cse110/cse110lab4part5/ServerAPI.java
+++ app/src/main/java/edu/ucsd/cse110/cse110lab4part5/ServerAPI.java
@@ -2,8 +2,12 @@ package edu.ucsd.cse110.cse110lab4part5;
 
 import android.util.Log;
 
+import androidx.annotation.AnyThread;
+import androidx.annotation.WorkerThread;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -24,7 +28,11 @@ public class ServerAPI {
         this.client = new OkHttpClient();
     }
 
-    public static ServerAPI provide() {
+    /**
+     * Singleton getinstance
+     * @return instance of the ServerAPI
+     */
+    public static ServerAPI getInstance() {
         if (instance == null) {
             instance = new ServerAPI();
         }
@@ -32,43 +40,25 @@ public class ServerAPI {
     }
 
     /**
-     * An example of sending a GET request to the server.
-     *
-     * The /echo/{msg} endpoint always just returns {"message": msg}.
+     * Get a friend from the server
+     * @param uuid of the friend
+     * @return a friend from the server
      */
-    public void echo(String msg) {
-        // URLs cannot contain spaces, so we replace them with %20.
-        msg = msg.replace(" ", "%20");
-
-        Request request = new Request.Builder()
-                .url("https://sharednotes.goto.ucsd.edu/echo/" + msg)
-                .method("GET", null)
-                .build();
-
-        try (okhttp3.Response response = client.newCall(request).execute()) {
-            assert response.body() != null;
-            String body = response.body().string();
-            Log.i("ECHO", body);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
+    @WorkerThread
     public Friend getFriend(String uuid){
         // URLs cannot contain spaces, so we replace them with %20.
         uuid = uuid.replace(" ", "%20");
 
 
         Request request = new Request.Builder()
-                .url("https://sharednotes.goto.ucsd.edu/notes/" + uuid)
+                .url("https://socialcompass.goto.ucsd.edu/location/" + uuid)
                 .method("GET", null)
                 .build();
 
         try (okhttp3.Response response = client.newCall(request).execute()) {
             assert response.body() != null;
             String body = response.body().string();
-            if(body.contains("Note not found.")){
+            if(body.contains("Location not found")){
                 Log.i("getFriend", "Friend " + uuid + " not found in database: ");
                 return null;
             }
@@ -81,28 +71,106 @@ public class ServerAPI {
         return null;
     }
 
-    public void upsertFriend(String uuid, String json){
+    /**
+     * Async call the server to get a friend's info.
+     * @param uuid of the friend
+     * @return a future which will contain the friend object upon thread completion
+     */
+    @AnyThread
+    public Future<Friend> getFriendAsync(String uuid) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Friend> future = executor.submit(() -> getFriend(uuid));
 
+        // We can use future.get(1, SECONDS) to wait for the result.
+        return future;
+    }
+
+    /**
+     * Upsert a friend from the server
+     * @param uuid of the friend
+     * @return the http response body
+     */
+    @WorkerThread
+    public String upsertFriend(String uuid, String json){
         // URLs cannot contain spaces, so we replace them with %20.
         uuid = uuid.replace(" ", "%20");
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        String finalTitle = uuid;
-        executor.submit(()->{
-            RequestBody body = RequestBody.create(json, JSON);
-            Request request = new Request.Builder()
-                    .url("https://sharednotes.goto.ucsd.edu/notes/" + finalTitle)
-                    .put(body)
-                    .build();
-
-            try (okhttp3.Response response = client.newCall(request).execute()) {
-                assert response.body() != null;
-                String responseBody = response.body().string();
-                Log.i("UpsertFriend", "Upserted friend with response: " + responseBody);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url("https://socialcompass.goto.ucsd.edu/location/" + uuid)
+                .put(body)
+                .build();
+        try (okhttp3.Response response = client.newCall(request).execute()) {
+            assert response.body() != null;
+            String responseBody = response.body().string();
+            Log.i("UpsertFriend", "Upserted friend with response: " + responseBody);
+            return responseBody;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
 
     }
+
+    /**
+     * Async call the server to upsert user info
+     * @param uuid of the user
+     * @return a future which will contain the response body from the server upon thread completion
+     */
+    @AnyThread
+    public Future<String> upsertFriendAsync(String uuid, String json) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<String> future = executor.submit(() -> upsertFriend(uuid, json));
+
+        // We can use future.get(1, SECONDS) to wait for the result.
+        return future;
+    }
+
+    /**
+     * Check if a uuid exists on the server already
+     * @param uuid to check
+     * @return boolean true/false for existance, true if no server connection since this is used
+     * to determine if a UUID is taken when making a new one, so err on side of caution.
+     */
+    @WorkerThread
+    public boolean uuidTaken(String uuid){
+        // URLs cannot contain spaces, so we replace them with %20.
+        uuid = uuid.replace(" ", "%20");
+
+
+        Request request = new Request.Builder()
+                .url("https://socialcompass.goto.ucsd.edu/location/" + uuid)
+                .method("GET", null)
+                .build();
+
+        try (okhttp3.Response response = client.newCall(request).execute()) {
+            assert response.body() != null;
+            String body = response.body().string();
+            if(body.contains("Location not found")){
+                Log.i("UUID check", uuid + " not found in server");
+                return false;
+            }
+            Log.i("UUID check", "UUID exists in server: " + body);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return false;
+    }
+
+    /**
+     * Async check if uuid exists in the server already
+     * @param uuid to check
+     * @return a future which will contain the result of checking the server
+     */
+    @AnyThread
+    public Future<Boolean> uuidTakenAsync(String uuid) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Boolean> future = executor.submit(() -> uuidTaken(uuid));
+
+        // We can use future.get(1, SECONDS) to wait for the result.
+        return future;
+    }
+
+
 }
